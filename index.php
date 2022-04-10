26e2fbb80268cca95a3a21f6d2e2a62433e419bd
AgarwalAayushi <63318339+AgarwalAayushi@users.noreply.github.com>
2022-04-10 15:04:39 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Alumni Management System</title>
+<link rel="stylesheet" href="css/header_navigationbar.css" />
+<link rel="stylesheet" href="css/index.css" />
+<?php
+include_once"setting/index_navigation.php";
+?>
+
+</head>
+<body>
+<br><br>
+<div id="welcome">
+
+<script src="javascript/index.js"></script>
+
+
+<p class="p1">Welcome to Alumni Management System</p>
+<p class="p2">Let's keep in touch.. <br></p>
+<p class="p3">Are you missing them? Your coursemates, your squad, your friends who up all night
+   with you for assignments, gathering, events and so on.
+<br><br>
+Welcome back dearest alumni. Genuinely, one of our greatest assets is our global network of
+alumni. This page is to celebrate your achievements and provides you an access to our alumni
+community.Let's discover how to get involved and connect with the University and its
+ supporters. Learn about the events we hold, and our services.</p>
+<br>
+<br />
+</div>
+</body>
+</html>
